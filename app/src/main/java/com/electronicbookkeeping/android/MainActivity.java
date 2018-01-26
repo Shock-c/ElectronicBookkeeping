d4df4e03a6a5bbea0d90e6c698deec30d1efe93f
shock <shock@gmail.com>
2018-01-26 11:02:31 +0800

    主界面完成，完善解锁

--- app/src/main/java/com/electronicbookkeeping/android/MainActivity.java
+++ app/src/main/java/com/electronicbookkeeping/android/MainActivity.java
@@ -1,6 +1,8 @@
 package com.electronicbookkeeping.android;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,27 +18,43 @@ import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
+    MyApplication myApplication;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText setpswtext = (EditText) findViewById(R.id.edit_setpsw);
-        Button setpsw = (Button) findViewById(R.id.button_setpsw);
+        Button bookkeep = (Button) findViewById(R.id.button_bookkeep);
+        Button statictics = (Button) findViewById(R.id.button_statistics);
+        Button password = (Button) findViewById(R.id.button_password);
 
-        setpsw.setOnClickListener(new View.OnClickListener() {
+        bookkeep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DataSupport.deleteAll(Password.class);
-                Password password = new Password();
-                password.setPwd(setpswtext.getText().toString());
-                password.save();
 
-                setpswtext.setText("");
+            }
+        });
+
+        statictics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
     }
 
-
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        myApplication.isLocked = true;
+    }
 }
---
+++ app/src/main/java/com/electronicbookkeeping/android/activity/BookkeppActivity.java
@@ -0,0 +1,15 @@
+package com.electronicbookkeeping.android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.electronicbookkeeping.android.R;
+
+public class BookkeppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookkepp);
+    }
+}
